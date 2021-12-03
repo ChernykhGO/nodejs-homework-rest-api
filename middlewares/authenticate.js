87493879a2bc327295d6bdb10f9a337053c512a0
Chernykh Oksana <spec20@ukr.net>
2021-12-03 02:42:38 +0200

    added Error if not authorized

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -5,9 +5,9 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
-  // if (!req.headers.authorization) {
-  //   throw new Unauthorized("Error, not authorized");
-  // }
+  if (!req.headers.authorization) {
+    throw new Unauthorized("Error, not authorized");
+  }
   try {
     const [bearer, token] = req.headers.authorization.split(" ");
     if (bearer !== "Bearer" || req.headers.authorization === undefined) {
